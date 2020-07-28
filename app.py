db1f581d5f09f8a936e3ad365db60e3c3e101415
myname <cristina.chiticari@gmail.com>
2020-07-28 15:32:03 -0400

    init

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author' : 'Kristy K',
+        'title' : 'Blog Post',
+        'content': 'First post',
+        'date': 'July 28' 
+    },
+      {
+        'author' : 'Kristy K',
+        'title' : 'Blog Post 2',
+        'content': 'Second post',
+        'date': 'July 28' 
+    }
+]
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+    app.run(debug= True)
