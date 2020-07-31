6ee69601873ca370193fa8894f07248f1520a96e
myname <cristina.chiticari@gmail.com>
2020-07-31 13:59:37 -0400

    login

--- app.py
+++ app.py
@@ -35,9 +35,15 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been loggen in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username amd password', 'danger')
     return render_template('login.html', title='Logim', form=form)
 
 if __name__ == '__main__':
