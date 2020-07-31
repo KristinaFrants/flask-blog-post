874850ffa5736d0151e9c4a5ced0a38ed2d54de8
myname <cristina.chiticari@gmail.com>
2020-07-31 12:53:33 -0400

    forms new

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for
-
-
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY']= 'b01a7e5955100c3ebf1083e2b805c4af'
 
 posts = [
     {
@@ -25,6 +26,16 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Logim', form=form)
 
 if __name__ == '__main__':
     app.run(debug= True)
