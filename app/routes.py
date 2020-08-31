c54409164f64ae09e42d9092d70a07973a6e7ffd
myname <cristina.chiticari@gmail.com>
2020-08-31 15:03:30 -0400

    posts articles

--- app/routes.py
+++ app/routes.py
@@ -3,28 +3,16 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from app import app, db, bcrypt
-from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
 
-posts = [
-    {
-        'author' : 'Kristy K',
-        'title' : 'Blog Post',
-        'content': 'First post',
-        'date': 'July 28' 
-    },
-      {
-        'author' : 'Kristy K',
-        'title' : 'Blog Post 2',
-        'content': 'Second post',
-        'date': 'July 28' 
-    }
-]
+
 @app.route('/')
 @app.route('/home')
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 @app.route('/about')
@@ -96,3 +84,16 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post',
+                           form=form, legend='New Post')
