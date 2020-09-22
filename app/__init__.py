89ae32efbafe2fa59005722e74f0c44fac091500
myname <cristina.chiticari@gmail.com>
2020-09-22 17:57:58 -0400

    custom error pages

--- app/__init__.py
+++ app/__init__.py
@@ -26,8 +26,10 @@ def create_app(config_class=Config):
     from app.users.routes import users
     from app.posts.routes import posts
     from app.main.routes import main
+    from app.errors.handlers import errors
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
